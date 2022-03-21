543722b644c29104b19826f125586710eaf4c25c
taichi <taicloud0803@icloud.com>
2022-03-22 01:42:48 +0900

    Initial Commit

---
+++ World Apps/World_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_AppsApp.swift
+//  World Apps
+//
+//  Created by taichi on 2022/03/22.
+//
+
+import SwiftUI
+
+@main
+struct World_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
