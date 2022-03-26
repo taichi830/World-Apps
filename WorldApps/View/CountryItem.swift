0daab59728f23e42b5397a16f28bcde846b65f97
taichi <taicloud0803@icloud.com>
2022-03-26 13:06:31 +0900

    国を選択時、選択されてないViewに白Viewをかけるようにした

--- WorldApps/View/CountryItem.swift
+++ WorldApps/View/CountryItem.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct CountryItem: View {
     var country: Country
+    var isSelected: Bool
     var body: some View {
         ZStack(alignment: .bottomLeading) {
             Image(country.imageName.rawValue)
                 .resizable()
                 .scaledToFill()
                 .frame(width: 120, height: 80)
-                .overlay(TextOverlay(country: country))
+                .overlay(TextOverlay(country: country, isSelected: isSelected))
                 .cornerRadius(10)
         }
     }
@@ -23,8 +24,12 @@ struct CountryItem: View {
 
 struct TextOverlay: View {
     var country: Country
+    var isSelected: Bool
     var gradient: LinearGradient {
         LinearGradient.init(gradient: Gradient(colors: [country.color.opacity(0.9), country.color.opacity(0.4)]), startPoint: .bottomLeading, endPoint: .topTrailing)
+    }
+    var whiteGradient: LinearGradient {
+        LinearGradient.init(gradient: Gradient(colors: [.white.opacity(0.7)]), startPoint: .bottomLeading, endPoint: .topTrailing)
     }
     
     var body: some View {
@@ -32,9 +37,12 @@ struct TextOverlay: View {
             gradient
             VStack(alignment: .leading) {
                 Text(country.name)
-                    .font(.system(size: 25, weight: .semibold))
+                    .font(.system(size: 20, weight: .semibold))
             }
             .padding(EdgeInsets(top: 0, leading: 10, bottom: 5, trailing: 0))
+            if !isSelected {
+                whiteGradient
+            }
         }
         .foregroundColor(.white)
     }
@@ -42,7 +50,7 @@ struct TextOverlay: View {
 
 struct CountryItem_Previews: PreviewProvider {
     static var previews: some View {
-        CountryItem(country: Country(country: .korea, name: "Korea", imageName: .korea, color: .blue))
+        CountryItem(country: Country(country: .korea, name: "Korea", imageName: .korea, color: .blue), isSelected: false)
             .previewLayout(.fixed(width: 200, height: 100))
     }
 }
--- WorldApps/View/CountryList.swift
+++ WorldApps/View/CountryList.swift
@@ -10,19 +10,28 @@ import SwiftUI
 struct CountryList: View {
     var countryData: CountryData
     var viewModel: AppViewModel
+    @State var selectedIndex:Int = 0
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .top, spacing: 8) {
                 ForEach(0..<countryData.data.count) { index in
-                    CountryItem(country: countryData.data[index])
-                        .onTapGesture {
-                            viewModel.navigationTitle = "Top50: \(countryData.data[index].name)"
-                            viewModel.country =  countryData.data[index].country
-                            viewModel.tapEvent.send(())
-                        }
+                    if selectedIndex == index {
+                        CountryItem(country: countryData.data[index], isSelected: true)
+                    }else {
+                        CountryItem(country: countryData.data[index], isSelected: false)
+                            .onTapGesture {
+                                let feedBack = UIImpactFeedbackGenerator(style: .heavy)
+                                feedBack.impactOccurred()
+                                selectedIndex = index
+                                viewModel.navigationTitle = "Top50: \(countryData.data[index].name)"
+                                viewModel.country =  countryData.data[index].country
+                                viewModel.tapEvent.send(())
+                            }
+                    }
+                        
                 }
             }
-            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 0))
+            .padding(EdgeInsets(top: 15, leading: 10, bottom: 15, trailing: 0))
         }
     }
 }
@@ -30,6 +39,6 @@ struct CountryList: View {
 struct CountryList_Previews: PreviewProvider {
     static var previews: some View {
         CountryList(countryData: CountryData(), viewModel: AppViewModel())
-            .previewLayout(.fixed(width: 375, height: 200))
+            .previewLayout(.fixed(width: 375, height: 100))
     }
 }
